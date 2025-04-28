f861d0e058d73ec293c7a6a60903d4dc56c0e955
Vinícius Madureira Godinho de Oliveira <vinimagod@gmail.com>
2025-04-28 19:34:32 -0300

    primeiro commit

---
+++ src/main/java/com/vmgo/reserva/ReservaService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vmgo.reserva;
+
+/**
+ *
+ * @author VINICIUS
+ */
+public class ReservaService {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
